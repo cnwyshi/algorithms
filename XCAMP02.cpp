d0b73b7d6a47f2695528f8e571262bd72d847904
genius <genius@imac.attlocal.net>
2022-10-04 08:14:29 -0700

    add admission work

---
+++ XCAMP02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int b;
+    cin >> b;
+    int n;
+    cin >> n;
+    int a = b%n;
+    cout << a << endl;
+    return 0;
+}
---
+++ XCAMP08.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    vector<int> v;
+    for (int i = 0; i < (n + m); i++) {
+        int k;
+        cin >> k;
+        v.push_back(k);
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
