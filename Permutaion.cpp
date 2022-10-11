312b2d1d8529056adb42609368798b6dafd9f57c
genius <genius@imac.attlocal.net>
2022-10-10 20:32:11 -0700

    How to Get Permutaions of Size N from M Numbers

---
+++ Permutaion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void permutations(int m, vector<int>& a, int j, vector<int>& vis) {
+    int n = a.size();
+    if (j == n) {
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 1; i <= m; i++) {
+        if (vis[i] != 0) {
+            continue;
+        }
+        vis[i] = 1;
+        a[j] = i;
+        permutations(m, a, j + 1, vis);
+        vis[i] = 0;
+    }
+}
+
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    vector<int> visited(m);
+    vector<int> nums(n);
+    permutations(m, nums, 0, visited);
+}
