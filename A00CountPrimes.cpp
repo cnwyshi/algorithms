309454f13c1e74e9fbb3e33b856c68e503904bc4
WilliamS <73660888+William4023@users.noreply.github.com>
2022-10-23 21:13:04 -0700

    Create A00CountPrimes.cpp

---
+++ A00CountPrimes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // Time: O(N * sqrt(N))
+    /* Steps:       n = 100, ans = 25
+    */
+    int countPrimes(int n) {
+        vector<bool> composite (n + 1);
+        int ans = 0;
+        for(int i = 2; i < n; i++){
+            if(composite[i]){
+                continue;
+            }
+            for(int j = i*2; j < n; j+=i){
+                composite[j] = true;
+            }
+        }
+        for(int i = 2; i < n; i ++){
+            if(composite[i] == false){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+/*
+100 
+2, 3 , 4 ..... 100
+2  no more 4 6 8 10 12 14... 100
+3 no more 6 9 ... 99
+*/
