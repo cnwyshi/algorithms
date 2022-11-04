0a71b416501d2220f43dc6fb35f636a2afe1168a
Your Name <you@example.com>
2022-11-03 23:25:35 -0700

    add W03Q03V2

---
+++ W03Q03V2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void permutations(vector<int> &nums, vector<int> &visited, int j, int last, int &ans) {
+    int n = nums.size();
+    if (j == n) {
+        ans++;
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 0) {
+            if (j == 0 || abs(i + 1 - last) > 1) {
+                visited[i] = 1;
+                // nums[j] = i + 1;
+                permutations(nums, visited, j + 1, i + 1, ans);
+                visited[i] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, ans = 0;
+    cin >> n;
+    vector<int> nums(n), visited(n);
+    permutations(nums, visited, 0, -1, ans);
+    cout << ans << endl;
+}
+
+/*
+5
+*/
