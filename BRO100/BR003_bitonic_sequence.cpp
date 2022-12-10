471b635662d5be5d20a27502cbd720e5dcd0738e
genius <genius@imac.attlocal.net>
2022-12-10 10:10:13 -0800

    Double Sort Sequence

--- BRO100/BR003_bitonic_sequence.cpp
+++ BRO100/BR003_bitonic_sequence.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
 int main() {
-    return 0;
+    int  n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i ++){
+        cin >> nums[i];
+    }
+    sort(nums.begin(), nums.end());
+    int i = 0, j = n-1;
+    while(i <= j){
+        cout << nums[j] << endl;
+        j--;
+//        cout << i << " " << j << endl;
+        if(i < j){
+            cout << nums[i] << endl;
+            i++;
+//            cout << i << " " << j << endl;
+        }
+    }
 }
+/*
+5
+10
+-1
+3
+3
+-9
+ */
--- HW11_Q01_numbers_of_0_and_1.cpp
+++ HW11_Q01_numbers_of_0_and_1.cpp
@@ -12,6 +12,9 @@ void dfs(string &st, int count){
     if(st.size()== n){
         vec.push_back(st);
     }
+    if(vec.size() >= i){
+        return;
+    }
     string s0 = st + "0";
     dfs(s0, count);
     string s1 = st + "1";
@@ -21,5 +24,5 @@ int main() {
     cin >> n >> l >> i;
     string st;
     dfs(st, 0);
-    cout << vec[i - 1];
+    cout << vec[i - 1];// cout << vec.at(i-1);
 }
