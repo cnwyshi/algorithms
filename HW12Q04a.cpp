0793aa955ba6baf22c082cb4a7c3e26ea650ec8e
Your Name <you@example.com>
2022-12-19 09:07:31 -0800

    add usaco 2019 feb02

---
+++ HW12Q04a.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void dfs(int n, int curr, int i, int s, vector<string>& ops) {
+    if (i == n) {
+        if (s == 0) {
+            for (int j = 0; j < n; j ++) {
+                cout << (j + 1) << ops[j];
+            }
+            cout << endl;
+        }
+        return;
+    }
+    ops[i] = "+";
+    dfs(n, i + 1, s, ops);
+    ops[i] = "-";
+    dfs(n, i + 1, s, ops);
+    ops[i] = "";
+    dfs(n, i + 1, s, ops);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> ops(n);
+    dfs(n, 0, 0, 0, ops);
+    return 0;
+}
+
+/*
+n = 7
+1+2+3
+
+i = 0, s = 0 -> s = 1
+
+i = 1, p = 1
+  i = 2
+      1 + 2
+      1 - 2
+      12
+
+      1-3
+dfs(1)
+ 1
+ - s*10-i
+ +
+
+*/
---
+++ USACO_2019_Bronze_Feb02_GreateRevegetate.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    ifstream  ifs("revegetate.in");
+    ifs >> n >> m;
+
+//    vector<vector<int>> graph(n + 1)
+    int cow[m][2];
+    int a[m], b[m], ans[n + 1];
+    for (int i = 0; i < m; i ++) {
+//        ifs >> cow[i][0] >> cow[i][1];
+//        if (cow[i][0] > cow[i][1]) {
+//            swap(cow[i][])
+//        }
+        ifs >> a[i] >> b[i];
+        if (a[i] > b[i]) {
+            swap(a[i], b[i]);
+        }
+    }
+
+    ofstream  ofs("revegetate.out");
+    for (int y = 1; y <= n; y ++) {
+        int c = 0;
+        for (c = 1; c <= 4; c ++) {
+            bool match = true;
+            for (int i = 0; i < m; i ++) {
+                if (b[i] == y) {
+                    int x = a[i];
+                    if (ans[x] == c) {
+                        match = false;
+                    }
+                }
+            }
+            if (match) {
+                break;
+            }
+        }
+        ans[y] = c;
+        ofs << c;
+    }
+    return 0;
+}
+
+/*
+5 6
+4 1
+4 2
+4 3
+2 5
+1 2
+1 5
+*/
