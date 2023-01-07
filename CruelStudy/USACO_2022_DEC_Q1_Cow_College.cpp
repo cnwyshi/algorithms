f4226d1ac6bf094c5707be35e17d4e115a06efa7
Your Name <you@example.com>
2023-01-06 21:34:31 -0800

    USACO 2022 Dec Q1 Cow College

---
+++ CruelStudy/USACO_2022_DEC_Q1_Cow_College.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> tuition (n);
+    for(int i = 0; i < n; i ++){
+        cin >> tuition[i];
+    }
+    sort(tuition.begin(), tuition.end());
+    ll ans = 0;
+    int best_charge = 0;
+    for(int i = 0; i < n; i++){
+        ll cows = n-i;
+        ll current = tuition[i] * cows;
+        ll replace = ans;
+        ans = max(current, ans);
+        if(ans != replace){
+            best_charge = tuition[i];
+        }
+    }
+    cout << ans << " " << best_charge << endl;
+}
+/*
+4
+1 6 4 6
+*/
