a6c12bff38402973e2b7bbde8a410c597713ce8f
Your Name <you@example.com>
2023-01-06 21:09:53 -0800

    Usaco 2022 Open Q3 using DFS

---
+++ CruelStudy/USACO_2022_OPEN_Q1_Photoshoot.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, ans = 0;
+    char last;
+    string s;
+    cin >> n >> s;
+    for(int i = 0; i < n; i+=2){
+        if(s[i] != s[i+1]){
+            if(s[i] != last){
+                ans += 1;
+                last = s[i];
+            }
+        }
+    }
+    if(last == 'H'){
+        ans -= 1;
+    }
+    cout << ans << endl;
+}
---
+++ CruelStudy/USACO_2022_OPEN_Q3_Alchemy.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vvi=vector<vector<int>>;
+using vi = vector<int>;
+
+bool make(vi& count, vvi& recipies, int t){
+  if(count[t]>0){
+    count[t]--;
+    return true;
+  }
+  if(recipies[t].empty()){
+    return false;
+  }
+  for(int i:recipies[t]){
+    if(!make(count, recipies, i)){
+      return false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  int ans = 0;
+  int n;
+  cin >> n;
+  vi met(n);
+  for(int i = 0; i<n; i++){
+    cin >> met[i];
+  }
+  int potions;
+  cin >> potions;
+  vvi rec(n);
+  for(int i = 0; i < potions; i++){
+    int l, m;
+    cin >> l >> m;
+    for(int j = 0; j < m; j++){
+      int metal;
+      cin >> metal;
+      rec[l-1].push_back(metal-1);
+    }
+  }
+  while(make(met, rec, n-1)){
+    ans++;
+  }
+  cout << ans << endl;
+}
+/*
+5
+2 0 0 1 0
+3
+5 2 3 4
+2 1 1
+3 1 2
+*/
