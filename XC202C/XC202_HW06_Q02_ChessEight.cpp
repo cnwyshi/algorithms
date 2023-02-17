0530087a743212a11de5c5e4f3af85ad57429b8b
Your Name <you@example.com>
2023-02-17 08:35:27 -0800

    try both single and dual BFS

--- XC202C/XC202_HW06_Q02_ChessEight.cpp
+++ XC202C/XC202_HW06_Q02_ChessEight.cpp
@@ -10,40 +10,24 @@ int main() {
     vector<vector<int>> move = {{1, 3},{0, 2, 4},{1,5},{0,4,6},{1,3,5,7},{2,4,8},{3,7},{4,6,8},{5,7}};
     string board, target = "123804765";
     cin >> board;
-    map<string, int> front, back;
-    queue<string> q1, q2;
+    map<string, int> front;
+    queue<string> q1;
     q1.push(board);
-    q2.push(target);
-    front[board] = back[target] = 0;
-    while (!q1.empty() || !q2.empty()) {
+    front[board] = 0;
+    while (!q1.empty()) {
         string s = q1.front();
         q1.pop();
+        if (s == target) {
+            cout << front[s] << endl;
+            return 0;
+        }
         int i = s.find('0');
         for (int j : move[i]) {
             string t = s;
             swap(t[i], t[j]);
-            if (front.find(t) != front.end()) {
+            if (front.find(t) == front.end()) {
                 q1.push(t);
                 front[t] = front[s] + 1;
-            }
-            if (back.find(t) != back.end()) {
-                cout << front[t] + back[t] << endl;
-                return 0;
-            }
-        }
-        s = q2.front();
-        q2.pop();
-        i = s.find('0');
-        for (int j : move[i]) {
-            string t = s;
-            swap(t[i], t[j]);
-            if (back.find(t) != back.end()) {
-                q2.push(t);
-                back[t] = back[s] + 1;
-            }
-            if (front.find(t) != front.end()) {
-                cout << front[t] + back[t] << endl;
-                return 0;
             }
         }
     }
@@ -53,5 +37,6 @@ int main() {
 
 /*
 283104765
+123804765
 
  */
---
+++ XC202C/XC202_HW06_Q02_ChessEight_DualBFS.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+012
+345
+678
+*/
+int main() {
+    vector<vector<int>> move = {{1, 3},{0, 2, 4},{1,5},{0,4,6},{1,3,5,7},{2,4,8},{3,7},{4,6,8},{5,7}};
+    string board, target = "123804765";
+    cin >> board;
+    map<string, int> front, back;
+    queue<string> q1, q2;
+    q1.push(board);
+    q2.push(target);
+    front[board] = back[target] = 0;
+    while (!q1.empty() || !q2.empty()) {
+        string s = q1.front();
+        q1.pop();
+        if (back.find(s) != back.end()) {
+            cout << front[s] + back[s] << endl;
+            return 0;
+        }
+        int i = s.find('0');
+        for (int j : move[i]) {
+            string t = s;
+            swap(t[i], t[j]);
+            if (front.find(t) == front.end()) {
+                q1.push(t);
+                front[t] = front[s] + 1;
+            }
+            // cout << s << " -> " << t << " " << front[t] << endl;
+        }
+        s = q2.front();
+        q2.pop();
+        if (front.find(s) != front.end()) {
+            cout << front[s] + back[s] << endl;
+            return 0;
+        }
+        i = s.find('0');
+        for (int j : move[i]) {
+            string t = s;
+            swap(t[i], t[j]);
+            if (back.find(t) == back.end()) {
+                q2.push(t);
+                back[t] = back[s] + 1;
+            }
+            // cout << t << " <- " << s << " " << back[t] << endl;
+        }
+    }
+    cout << -1 << endl;
+    return 0;
+}
+
+/*
+283104765
+123804765
+
+ */
