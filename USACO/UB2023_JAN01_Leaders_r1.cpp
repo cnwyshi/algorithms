f9a98615f2931102ddcbf177887e2873e99e10bb
Your Name <you@example.com>
2023-02-25 00:14:12 -0800

    before change index to 0

--- USACO/UB2023_JAN01_Leaders_r1.cpp
+++ USACO/UB2023_JAN01_Leaders_r1.cpp
@@ -1,32 +1,47 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define int long long
 using namespace std;
-
 const int N=1e5+10;
-int n;
-char s[N]; int a[N];
-int main() {
-    scanf("%d%s", &n, s + 1);
-    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
-    int pos, last;
-    for (int i = 2; i <= n; i++) {
-        if (s[i] != s[1]) { //找位置靠后秘
-            pos = i;
-            break;
-        }
+int a[N];
+signed main() {
+    int n;
+    cin >> n;
+    string st;
+    cin >> st;
+    st = ' ' + st;
+    int g = 0, h = 0, G = 0, H = 0;
+    for (int i = 1; i < st.size(); i++) {
+        if (st[i] == 'G' && !g)g = i;
+        else if (st[i] == 'H' && !h)h = i;
+    }
+    for (int i = st.size() - 1; i >= 1; i--) {
+        if (st[i] == 'G' && !G)G = i;
+        else if (st[i] == 'H' && !H)H = i;
     }
     for (int i = 1; i <= n; i++) {
-        if (s[i] == s[1]) { //第一头秘类奶
-            last = i;
+        cin >> a[i];
+    }
+    char ch;
+    int ans = 0;
+    bool gg = 0, hh = 0;
+    for (int i = 1; i <= n; i++) {
+        if (st[i] == 'G') {
+            if (a[i] >= h && i <= h && a[h] >= H) {
+                ans++;
+                if (i == g)gg = 1;
+            }
+        }
+        if (st[i] == 'H') {
+            if (a[i] >= g && i <= g && a[g] >= G) {
+                ans++;
+                if (i == h)hh = 1;
+            }
         }
     }
-    int ans = 0;
-    for (int i = 1; i < pos; i++) {
-        if ((i == 1 && a[i] >= last) || a[i] >= pos)
-            ans++;
-        printf("%d", ans);
-        return 0;
-    }
+    if(a[g]>=G&&a[h]>=H&&!gg&&!hh)ans++;
+    cout<<ans;
 }
+
 //int main() {
 //    int n;
 //    string s;
