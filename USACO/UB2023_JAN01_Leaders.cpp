7fecf70eee64de60562ec7dd4f5218a46fd80c8c
genius <genius@imac.attlocal.net>
2023-02-27 17:11:27 -0800

    finish USACO 2023 JAN

--- USACO/UB2023_JAN01_Leaders.cpp
+++ USACO/UB2023_JAN01_Leaders.cpp
@@ -3,23 +3,57 @@
 #include <string>
 using namespace std;
 int main() {
-    int n;
+    int n, ans = 0;
     cin >> n;
     int fg, fh, lg, lh;
     string breed;
     cin >> breed;
     vector<int> e(n);
     for(int i = 0; i < n; i++){
-        cin >> e[i];
+        int x;
+        cin >> x;
+        x--;
+        e[i] = x;
     }
+    fg = breed.find("G");
+    fh = breed.find("H");
+    lg = breed.rfind("G");
+    lh = breed.rfind("H");
+    if(e[fg] >= lg){
+        for(int i = 0; i < fg; i++){
+            if(e[i] >= fg){
+                ans++;
+            }
+        }
+    }
+    if(e[fh] >= lh){
+        for(int i = 0; i < fh; i++){
+            if(e[i] >= fh){
+                ans++;
+            }
+        }
+    }
+    if((e[fg] >= lg && e[fh] >= lh) && (lg < fh || lh < lg)){
+        ans++;
+    }
+    cout << ans << endl;
 }
 /*
 3
 GGH
 2 3 3
 
-G G H H
-4 4 4 4
+2
+
+H G G H H
+2 4 4 3 4
+
+H H G G G
+1 3 4 4 4
+
+ GGHH
+ HHGG
+
 
 
 */
--- USACO/UB2023_JAN03_Moo_Operations.cpp
+++ USACO/UB2023_JAN03_Moo_Operations.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
+int main() {
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         string s;
         cin >> s;
         int n = s.size();
-        if(n < 3){
+        if (n < 3) {
             cout << -1 << endl;
-        }
-        else {
-            if (s.find("MOO") != s.npos) {
-                cout << n - 3 << endl;
-            } else if (s.find("MOM") != s.npos|| s.find("OOO") != s.npos){
-                cout << n - 2 << endl;
-            } else if (s.find("OOM") != s.npos) {
-                cout << n - 1 << endl;
-            }
-            else{
-                cout << -1 << endl;
-            }
+        } else if (s.find("MOO") != s.npos) {
+            cout << n - 3 << endl;
+        } else if (s.find("MOM") != s.npos || s.find("OOO") != s.npos) {
+            cout << n - 2 << endl;
+        } else if (s.find("OOM") != s.npos) {
+            cout << n - 1 << endl;
+        } else {
+            cout << -1 << endl;
         }
     }
 }
