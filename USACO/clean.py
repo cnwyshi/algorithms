a5f40bf8634951ba040c8cf021ac977c8ef9b76b
Your Name <you@example.com>
2023-03-03 17:15:43 -0800

    rename

--- USACO/clean.py
+++ USACO/clean.py
@@ -3,6 +3,7 @@ import os
 for i in os.listdir('.'):
     j = i.replace('Jan', 'JAN')
     j = j.replace('Feb', 'FEB')
-    j = j.replace('_Q', '_0')
+    j = j.replace('Dec', 'DEC')
+    j = j.replace('_0', '0')
     print(i + ' -> ' + j)
     os.rename(i, j)
