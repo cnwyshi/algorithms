62d6713adab3bef8c066eadce0846b73e4c8135a
genius <genius@Ruis-MBP.attlocal.net>
2023-04-08 11:50:28 -0700

    update TeamsCode

--- TeamCode_2023/Greatest_Common_Multiple.cpp
+++ TeamCode_2023/Greatest_Common_Multiple.cpp
@@ -1,46 +1,28 @@
-//
-// Created by genius on 4/2/23.
-//
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+
 int _gcd(int a, int b){
-    if (a == 0) return b;
-    if (b == 0) return a;
-    if (a == b) return a;
-    if (a > b) return _gcd(a-b, b);
-    return _gcd(a, b-a);
+    if (a == 0)
+        return b;
+    return _gcd(b % a, a);
 }
+
 int main(){
     int t;
     cin >> t;
     while(t--){
-        int a, b, c;
+        long long a, b, c;
         cin >> a >> b >> c;
-        int g = _gcd(a, b);
-        g = _gcd(g, c);
+        long long g = _gcd(a,b);
+        long long lcm = a*b/g;
+        g = _gcd(g,c);
         a = a/g;
         b = b/g;
         c = c/g;
-        bool even = (a % 2 == 0 || b % 2 == 0);
         long long ans = 0;
-        if(even){
-            if(c % 2 == 0){
-                ans = c-2;
-            }
-            else if(c%2 == 1){
-                ans = c-1;
-            }
-        }
-        else{
-            if(c % 2 == 0){
-                ans = c-1;
-            }
-            else if(c%2 == 1){
-                ans = c-2;
-            }
-        }
-        cout << ans*g << endl;
+        long long left = lcm%c;
+        ans = lcm-left;
+        cout << ans << endl;
     }
     return 0;
 }
