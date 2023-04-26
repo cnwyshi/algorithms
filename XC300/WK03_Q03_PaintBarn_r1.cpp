923bc7e18a2575db4fc262cebb41be49be352f96
Your Name <you@example.com>
2023-04-25 17:26:58 -0700

    update XC300

--- XC300/WK03_Q03_PaintBarn_r1.cpp
+++ XC300/WK03_Q03_PaintBarn_r1.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int diff[1000][1000];
-int barn[1001][1001];
+int diff[1001][1001];
+int barn[1002][1002];
 
 void print(int matrix[][1001], int m, int n) {
     for (int i = 0; i < m; i ++) {
@@ -30,24 +30,24 @@ int main() {
     cin >> n >> k;
     for (int i = 0; i < n; i ++) {
         cin >> x1 >> y1 >> x2 >> y2;
-        x1 --, y1 --, x2 --, y2 --;
-        if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0) {
-            printf("%d %d %d %d %d\n", i, x1, y1, x2, y2);
-        }
+//        if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0) {
+//            printf("%d %d %d %d %d\n", i, x1, y1, x2, y2);
+//        }
         diff[x1][y1] ++;
         diff[x1][y2] --;
         diff[x2][y1] --;
         diff[x2][y2] ++;
     }
     // print(prefix, 10, 10);
-    cout << __LINE__ << endl;
-    for (int i = 0; i < 1000; i ++) {
-        for (int j = 0; j < 1000; j ++) {
-            // barn[i+1][j+1] = barn[i][j + 1] + barn[i + 1][j] - barn[i][j] + diff[i][j];
-            cout << i + 1 << " " << j + 1 << endl;
-            if (barn[i + 1][j + 1] == k) {
-                ans ++;
-            }
+    // cout << __LINE__ << endl;
+    for (int i = 0; i <= 1000; i ++) {
+        for (int j = 0; j <= 1000; j ++) {
+            barn[i+1][j+1] = barn[i][j + 1] + barn[i + 1][j] - barn[i][j] + diff[i][j];
+            // cout << i + 1 << " " << j + 1 << endl;
+            ans += barn[i + 1][j + 1] == k;
+//            if (barn[i + 1][j + 1] == k) {
+//                ans ++;
+//            }
         }
     }
     // print(barn, 1001, 1001);
