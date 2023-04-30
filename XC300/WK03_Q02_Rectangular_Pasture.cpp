273e050ae4d81a9863df7b65cd9bfeabc11736a6
genius <genius@imac.attlocal.net>
2023-04-30 13:24:37 -0700

    before fix

--- XC300/WK03_Q02_Rectangular_Pasture.cpp
+++ XC300/WK03_Q02_Rectangular_Pasture.cpp
@@ -1,48 +1,155 @@
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+void print(vector<vector<int>> matrix, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            // cout << matrix[i][j] << " ";
+        }
+        // cout << endl;
+    }
+}
+/*
+         x1      x2
+A------B----------C
+|      |          |
+|      |          |
+D------E----------F
+|      |          | y1
+|      |          |
+|      |          | y2
+G------H----------I
+
+ rsum(x1, y1, x2, y2) = EFIH = ACIG - ACFD - ABHG + ABED
+= prefix[x2+1][y2+1] - prefix[x2+1][y1] - prefix[x1][y2+1] + prefix[x1][y1]
+
+
+
+
+
+*/
+long long rsum(vector<vector<int>>& prefix, int x1, int y1, int x2, int y2){
+    return prefix[x2+1][y2+1] - prefix[x2+1][y1] - prefix[x1][y2+1] + prefix[x1][y1];
+}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int n;
+    cin >> n;
+    map<int, int> x_index, y_index;
+    vector<vector<int>> pos(n, vector<int>(2));
+    for(int i = 0; i < n; i++){
+        cin >> pos[i][0] >> pos[i][1];
+        x_index[pos[i][0]] = 0;
+        y_index[pos[i][1]] = 0;
+    }
+    int index = 0;
+    for (std::map<int,int>::iterator it=x_index.begin(); it!=x_index.end(); ++it){
+        it -> second = index;
+        // cout << it->first << ":" << it -> second << ",";
+        index++;
+    }
+    // cout << endl;
+    index = 0;
+    for (std::map<int,int>::iterator it=y_index.begin(); it!=y_index.end(); ++it){
+        it -> second = index;
+        // cout << it->first << ":" << it -> second << ",";
+        index++;
+    }
+    for(int i = 0; i < n; i++){
+        pos[i][0] = x_index[pos[i][0]];
+        pos[i][1] = y_index[pos[i][1]];
+    }
+    // // cout << endl;
+    vector<vector<int>> ps(n+1, vector<int> (n+1));
+    for(int i = 0; i < n; i++){
+        ps[pos[i][0]+1][pos[i][1]+1] = 1;
+    }
+    print(ps, n+1, n+1);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            ps[i+1][j+1] += ps[i][j+1] + ps[i+1][j] - ps[i][j];
+        }
+    }
+    long long ans = 0;
+    print(ps, n+1, n+1);
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            int xi = pos[i][0], yi = pos[i][1], xj = pos[j][0], yj = pos[j][1];
+            int top = min(xi, xj);
+            int bottom = max(xi, xj);
+            int left = min(yi, yj);
+            int right = max(yi, yj);
+            ans += rsum(ps, 0, top, left, bottom) * rsum(ps, right, top, n-1, bottom);
+            // // cout << top << " " << bottom << " " << rsum(ps, 0, top, left, bottom) * rsum(ps, right, top, n-1, bottom) << endl;
+        }
+    }
+    cout << ans+1 << endl;
 }
 /*
-4 3
-.*..
-*.**
-**..
-****
-2 2 3 4
-3 1 3 1
-1 1 2 2
+4
+0 2
+1 0
+2 3
+3 5
+      left         right
+A------B----------C-------------J
+|      |          |             |
+|      |          |             |
+D------E----------F-------------K top
+| 1    |          |             |
+|   1  |          |  1    1     |
+|      |          |    1        |
+G------H----------I-------------L bottom
+|      |          |             |
+|      |          |             |
+|      |          |             |
+M------N----------O-------------P
+0 0
+0 3
+3 0
 
+4
+0 2
+1 0
+2 3
+3 5
+
+x_index={0:0, 1:0, 2:0, 3:0}
+y_index={0:0, 2:0, 3:0, 5:0}
+
+x_index={0:0, 1:1, 2:2, 3:3}
+y_index={0:0, 2:1, 3:2, 5:3}
+
+0 0 1 0 0 0
+1 0 0 0 0 0
+0 0 0 1 0 0
+0 0 0 0 0 1
+
+ 0 0
+
+4
+0 1, i = 0
+1 0
+2 2, j = 2
+3 3
 0 1 0 0
-1 0 1 1
-1 1 0 0
-1 1 1 1
+1 0 0 0
+0 0 1 0
+0 0 0 1
+
+i = 0
+j = 2
+
+top = min(xi, xj)
+bottom = max(xi, xj)
+left = min(yi, yj)
+right = max(yi, yj)
+
+rsum(0, left, top, bottom) * rsum(right, n, top, bottom)
 
 0 0 0 0 0
 0 0 1 1 1
+0 1 2 2 2
+0 1 2 3 3
 0 1 2 3 4
-0 2 4 5 6
-0 3 6 8 10
-
-1 1 2 3
-2 0 2 0
-0 0 1 1
-
-A-----B---C
-|     |   |
-D-----E---F y1
-|     |   |
-G-----H---I y2
-      x1  x2
-EFHI = I - F - H + E
-= ps[x2+1][y2+1] - ps[x2+1][y2] - ps[x1][y2+1] + ps[x1+1][y1+1]
-E = x1, y1
-I = x2, y2
-H = x1, y2
-F = x2, y1
-
-6 -
-3 1 2
 */
