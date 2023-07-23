ccf9d5822ff8977128ddd6e5af4cc4370e6ce330
genius <genius@Ruis-MacBook-Pro.local>
2023-07-23 09:32:53 -0700

    update XC302

---
+++ A00Lower_and_Upper_Bound.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, t;
+    cin >> n >> t;
+    int nums[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    int lo = 0, hi = n - 1;
+    while (lo < hi) {
+        int mid = (hi + lo) / 2;
+        if (nums[mid] >= t) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    cout << (nums[lo] == t ? lo+1 : 0)  << '\n';
+}
+
+/*
+Sample Input:
+7 4
+1 2 4 4 5 7 9
+Sample Output:
+3
+
+lo = 0; hi = 6; mid = 3; nums[mid] = 4 >= 4; hi = mid;
+lo = 0; hi = 3; mid = 1; nums[mid] = 2 < 4; low = mid+1;
+lo = 2; hi = 3; mid = 2; nums[mid] = 4 >= 4; hi = mid;
+lo = 2; hi = 2;
+
+Sample Input:
+3 2
+1 3 5
+Sample Output:
+0
+
+lo = 0; hi = 2; mid = 1; nums[mid] = 3; 3 >= 2; hi = mid;
+lo = 0; hi = 1; mid = 0; nums[mid] = 1; 1 < 2; low = mid+1;
+lo = 1; hi = 1;
+*/
---
+++ Calico01.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Return the sum of A and B.
+ *
+ * A: a non-negative integer
+ * B: another non-negative integer
+ */
+int solve(vector<int> sides) {
+    map<int, int> dict;
+    for(int i : sides){
+        dict[i]++;
+    }
+    int ans = 0;
+    int ans2 = 0;
+    for(auto i : dict){
+        int copy = ans;
+        ans = max(ans, i.first*i.second);
+        if(ans > copy){
+            for(int j = 0; j < sides.size(); j++){
+                if(sides[j] == i.first){
+                    ans2 = j;
+                }
+            }
+        }
+    }
+    return ans2;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int n;
+        cin >> n;
+        vector<int> sides(n);
+        for(int j = 0; j < n; j++){
+            cin >> sides[j];
+        }
+        cout << solve(sides) << '\n';
+    }
+}
