59b3fcbc4eab87fa627419e964d8d360d164afb6
WilliamS <73660888+cnwyshi@users.noreply.github.com>
2023-12-22 21:15:31 -0800

    Create XC202_Weight_of_Subtrees.cpp
    Alvin asked a problem

---
+++ XC202/XC202_Weight_of_Subtrees.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void dfs(vector<vector<int>>& graph, int i, int p, vector<int>& dp){
+	int children = 0;
+	for(int c : graph[i]){
+		if(c != p){
+			dfs(graph, c, i, dp);
+			children += dp[c];
+		}
+	}
+	dp[i] = 1 + children;
+}
+int main() {
+	int n;
+	cin >> n;
+	vector<vector<int>> graph(n+1);
+	vector<int> dp(n+1);
+	for(int i = 0; i < n-1; i++){
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	dfs(graph, 1, -1, dp);
+	for(int i = 1; i <= n; i++){
+		cout << dp[i] << endl;
+	}
+}
