ab1f58530170716c97a4ce6255d2052f24b03e01
Your Name <you@example.com>
2024-01-04 16:26:40 -0800

    fix stdc++.h for macos

---
+++ install_bits_stdc++.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/khaveesh/macOS-stdc.h/master/stdc%2B%2B.h 
+sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/bits
+sudo cp stdc++.h /Library/Developer/CommandLineTools/usr/include/bits/stdc++.h
+sudo chmod a+r /Library/Developer/CommandLineTools/usr/include/bits/stdc++.h
+
