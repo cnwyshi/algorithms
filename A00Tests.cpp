ced0d8515a88ce0caca3104492cac70d2a83ad57
WilliamS <cnwyshi@gmail.com>
2024-11-30 20:52:12 -0800

    usaco

--- A00Tests.cpp
+++ A00Tests.cpp
@@ -1,35 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int INF = 1e9;
 
-class DSU {
-    vector<int> root, size;
-    int count, max_value = 1;
-public:
-    DSU(int n) {
-        count = n;
-        root.resize(n);
-        size.resize(n, 1);
-        for(int i = 0; i < n; i++) {
-            root[i] = i;
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n, q; cin >> n >> q;
+    vector<string> s(n);
+    for(int i = 0; i < n; i++) cin >> s[i];
+    vector<int> group(n);
+    int group_no = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == "or"){
+            group_no++;
+        }
+        else{
+            if(i % 2 == 0){
+                group[i] = group_no;
+            }
         }
     }
-    int find(int i) {
-        return root[i] = (root[i] == i ? i : find(root[i]));
-    }
-    bool union_(int a, int b) {
-        int ra = find(a), rb = find(b);
-        if(ra == rb){
-            return false;
+    vector<int> first_false(group_no + 1, INF);
+    vector<int> last_false(group_no + 1, -1);
+    for(int i = 0; i < n; i += 2){
+        int g = group[i];
+        if(s[i] == "false"){
+            if(first_false[g] == INF){
+                first_false[g] = i;
+            }
+            last_false[g] = i;
         }
-        root[ra] = rb;
-        size[rb] += size[ra];
-        max_value = max(max_value, size[rb]);
-        count--;
-        return true;
     }
-};
-
-int main() {
-    int n;
-    cin >> n;
+    int total_first_true = INF, total_last_true = -1;
+    for(int i = 0; i <= group_no; i++){
+        if(first_false[i] == INF){
+            if(total_first_true == INF){
+                total_first_true = i;
+            }
+            total_last_true = i;
+        }
+    }
+    while(q--){
+        int l, r; cin >> l >> r;
+        --l; --r;
+        string t; cin >> t;
+        // we only need one true for whole statement to be true
+        int l_group = group[l], r_group = group[r];
+        if(total_first_true < l_group || total_last_true > r_group){
+            cout << (t == "true" ? 'Y' : 'N');
+            continue;
+        }
+        if(t == "true"){
+            cout << (first_false[l_group] < l || last_false[r_group] > r ? 'N' : 'Y');
+        }
+        else{
+            cout << 'Y';
+        }
+    }
 }
