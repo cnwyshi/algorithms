34f52af05111c989ab58288d5bb9ccecea9bddde
WilliamS <73660888+cnwyshi@users.noreply.github.com>
2025-02-01 14:33:43 -0800

    fix cmake build

--- install_bits_stdc++.sh
+++ install_bits_stdc++.sh
@@ -1,5 +1,5 @@
-wget https://raw.githubusercontent.com/khaveesh/macOS-stdc.h/master/stdc%2B%2B.h 
-sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/bits
-sudo cp stdc++.h /Library/Developer/CommandLineTools/usr/include/bits/stdc++.h
-sudo chmod a+r /Library/Developer/CommandLineTools/usr/include/bits/stdc++.h
+# wget https://raw.githubusercontent.com/khaveesh/macOS-stdc.h/master/stdc%2B%2B.h
+sudo mkdir -p /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/bits
+sudo cp stdc++.h /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/bits/stdc++.h
+sudo chmod a+r /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/bits/stdc++.h
 
