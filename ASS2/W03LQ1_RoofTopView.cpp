8c68df615e37e4958f7a2d552f84529df0bba1f3
WilliamS <cnwyshi@gmail.com>
2025-02-02 09:05:30 -0800

    summary: fix S2 W 3 LQ1 RooftopView issue: floodfill always returns 1 cause: it skips the floodfill when the neighbor is a '*' solution: skip dfs only when it is a '.' Alphastar Week 3 Problem 1 RoofTopView

--- ASS2/W03LQ1_RoofTopView.cpp
+++ ASS2/W03LQ1_RoofTopView.cpp
@@ -6,13 +6,13 @@ int xD[] = {-1, 0, 1, 0};
 int yD[] = {0, 1, 0, -1};
 int cnt = 0;
 
-void floodfill (int x, int y, vector<vector<char>>& building) {
+void floodfill (int x, int y, vector<vector<char> >& building) {
     cnt++;
     building[x][y] = '.';
     for(int i = 0; i < 4; i++) {
         int xx = x + xD[i];
         int yy = y + yD[i];
-        if(xx < 0 || xx >= h || yy < 0 || yy >= w || building[xx][yy] != '.') {
+        if(xx < 0 || xx >= h || yy < 0 || yy >= w || building[xx][yy] == '.') {
             continue;
         }
         // cout << xx << " " << yy << endl;
@@ -24,10 +24,10 @@ void floodfill (int x, int y, vector<vector<char>>& building) {
 
 int main() {
     cin >> w >> h;
-    vector<vector<char>> building(h, vector<char>(w));
+    vector<vector<char> > building(h, vector<char>(w));
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
-            cout << i << " " << j << endl;
+            // cout << i << " " << j << endl;
             cin >> building[i][j];
         }
     }
@@ -37,7 +37,7 @@ int main() {
             if(building[i][j] == '*') {
                 cnt = 0;
                 floodfill(i, j,building);
-                cout << i << " " << j << " " << cnt << endl;
+                // cout << i << " " << j << " " << cnt << endl;
                 ans = max(ans, cnt);
             }
         }
